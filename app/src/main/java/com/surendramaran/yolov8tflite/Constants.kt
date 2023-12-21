140aa44facbcebec35ea3342b03ca25b1354ec69
Surendra Maran <65958897+surendramaran@users.noreply.github.com>
2023-12-21 06:44:55 +0530

    Update Constants.kt

--- app/src/main/java/com/surendramaran/yolov8tflite/Constants.kt
+++ app/src/main/java/com/surendramaran/yolov8tflite/Constants.kt
@@ -1,6 +1,6 @@
 package com.surendramaran.yolov8tflite
 
 object Constants {
-    const val MODEL_PATH = "x.tflite"
+    const val MODEL_PATH = "model.tflite"
     const val LABELS_PATH = "labels.txt"
 }
