3dedf2b5db000034cb26d26dcb40d117c7ab8de2
Surendra Maran <surendramaran8@gmail.com>
2024-03-28 17:58:26 +0530

    improved NMS time

--- app/src/main/java/com/surendramaran/yolov8tflite/Detector.kt
+++ app/src/main/java/com/surendramaran/yolov8tflite/Detector.kt
@@ -2,7 +2,6 @@ package com.surendramaran.yolov8tflite
 
 import android.content.Context
 import android.graphics.Bitmap
-import android.media.FaceDetector.Face.CONFIDENCE_THRESHOLD
 import android.os.SystemClock
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.Interpreter
@@ -16,7 +15,6 @@ import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
-import kotlin.math.min
 
 class Detector(
     private val context: Context,
@@ -93,24 +91,40 @@ class Detector(
         val output = TensorBuffer.createFixedSize(intArrayOf(1 , numChannel, numElements), OUTPUT_IMAGE_TYPE)
         interpreter?.run(imageBuffer, output.buffer)
 
+
         val bestBoxes = bestBox(output.floatArray)
+        inferenceTime = SystemClock.uptimeMillis() - inferenceTime
+
+
         if (bestBoxes == null) {
             detectorListener.onEmptyDetect()
             return
         }
 
-        inferenceTime = SystemClock.uptimeMillis() - inferenceTime
         detectorListener.onDetect(bestBoxes, inferenceTime)
     }
 
     private fun bestBox(array: FloatArray) : List<BoundingBox>? {
 
         val boundingBoxes = mutableListOf<BoundingBox>()
+
         for (c in 0 until numElements) {
-            val confidences = (4 until numChannel).map { array[c + numElements * it] }
-            val cnf = confidences.max()
-            if (cnf > CONFIDENCE_THRESHOLD) {
-                val cls = confidences.indexOf(cnf)
+            // credit https://github.com/philipmortimer
+            var maxConf = -1.0f
+            var maxIdx = -1
+            var j = 4
+            var arrayIdx = c + numElements * j
+            while (j < numChannel){
+                if (array[arrayIdx] > maxConf) {
+                    maxConf = array[arrayIdx]
+                    maxIdx = j - 4
+                }
+                j++
+                arrayIdx += numElements
+            }
+
+            if (maxConf > CONFIDENCE_THRESHOLD) {
+                val cls = maxIdx
                 val clsName = labels[cls]
                 val cx = array[c] // 0
                 val cy = array[c + numElements] // 1
@@ -129,7 +143,7 @@ class Detector(
                     BoundingBox(
                         x1 = x1, y1 = y1, x2 = x2, y2 = y2,
                         cx = cx, cy = cy, w = w, h = h,
-                        cnf = cnf, cls = cls, clsName = clsName
+                        cnf = maxConf, cls = cls, clsName = clsName
                     )
                 )
             }
@@ -183,7 +197,7 @@ class Detector(
         private const val INPUT_STANDARD_DEVIATION = 255f
         private val INPUT_IMAGE_TYPE = DataType.FLOAT32
         private val OUTPUT_IMAGE_TYPE = DataType.FLOAT32
-        private const val CONFIDENCE_THRESHOLD = 0.75F
+        private const val CONFIDENCE_THRESHOLD = 0.3F
         private const val IOU_THRESHOLD = 0.5F
     }
 }
